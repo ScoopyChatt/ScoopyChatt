3e32371239731becb4fde3fe5d4cf56b9f8b5e51
ScoopyChatt <brandonwesleycarter@gmail.com>
2026-06-13 01:16:23 -0400

    Fix quote entity corruption in progress.jsx

--- apps/web/src/components/ui/progress.jsx
+++ apps/web/src/components/ui/progress.jsx
@@ -1,20 +1,20 @@
-&rdquo;use client&rdquo;
+"use client"
 
-import * as React from &rdquo;react&rdquo;
-import * as ProgressPrimitive from &rdquo;@radix-ui/react-progress&rdquo;
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-import { cn } from &rdquo;@/lib/utils&rdquo;
+import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      &rdquo;relative h-2 w-full overflow-hidden rounded-full bg-primary/20&rdquo;,
+      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
       className
     )}
     {...props}>
     <ProgressPrimitive.Indicator
-      className=&rdquo;h-full w-full flex-1 bg-primary transition-all&rdquo;
+      className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
   </ProgressPrimitive.Root>
 ))
