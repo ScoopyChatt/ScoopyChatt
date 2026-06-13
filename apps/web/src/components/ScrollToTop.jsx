df5f09305758a4d461e77dd117064ad66581b4bd
ScoopyChatt <brandonwesleycarter@gmail.com>
2026-06-13 01:17:24 -0400

    Fix quote entity corruption in ScrollToTop.jsx

--- apps/web/src/components/ScrollToTop.jsx
+++ apps/web/src/components/ScrollToTop.jsx
@@ -1,11 +1,11 @@
-import { useLocation } from &rsquo;react-router-dom&rsquo;;
-import { useLayoutEffect } from &rsquo;react&rsquo;;
+import { useLocation } from 'react-router-dom';
+import { useLayoutEffect } from 'react';
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
 
     useLayoutEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: &rsquo;instant&rsquo; });
+        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
     }, [pathname]);
 
     return null;
